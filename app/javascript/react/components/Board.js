03b63bb0b0be8aad57df925a376fd3150792e57f
Michael Catanese <michaelcatanese@catanescomputer.myfiosgateway.com>
2021-05-03 11:28:29 -0400

    formatted

--- app/javascript/react/components/Board.js
+++ app/javascript/react/components/Board.js
@@ -2,35 +2,68 @@ import React, { useState } from "react"
 import Square from "./Square"
 
 const Board = (props) => {
-
   const [selectedSquare, setSelectedSquare] = useState(null)
 
   let boardData = []
   for (let i = 1; i <= 8; i++) {
-
     boardData.unshift(
       <>
         <span>
-          <Square row={i} column="a" selectedSquare={selectedSquare} setSelectedSquare={setSelectedSquare}/>
-          <Square row={i} column="b" selectedSquare={selectedSquare} setSelectedSquare={setSelectedSquare}/>
-          <Square row={i} column="c" selectedSquare={selectedSquare} setSelectedSquare={setSelectedSquare}/>
-          <Square row={i} column="d" selectedSquare={selectedSquare} setSelectedSquare={setSelectedSquare}/>
-          <Square row={i} column="e" selectedSquare={selectedSquare} setSelectedSquare={setSelectedSquare}/>
-          <Square row={i} column="f" selectedSquare={selectedSquare} setSelectedSquare={setSelectedSquare}/>
-          <Square row={i} column="g" selectedSquare={selectedSquare} setSelectedSquare={setSelectedSquare}/>
-          <Square row={i} column="h" selectedSquare={selectedSquare} setSelectedSquare={setSelectedSquare}/>
+          <Square
+            row={i}
+            column="a"
+            selectedSquare={selectedSquare}
+            setSelectedSquare={setSelectedSquare}
+          />
+          <Square
+            row={i}
+            column="b"
+            selectedSquare={selectedSquare}
+            setSelectedSquare={setSelectedSquare}
+          />
+          <Square
+            row={i}
+            column="c"
+            selectedSquare={selectedSquare}
+            setSelectedSquare={setSelectedSquare}
+          />
+          <Square
+            row={i}
+            column="d"
+            selectedSquare={selectedSquare}
+            setSelectedSquare={setSelectedSquare}
+          />
+          <Square
+            row={i}
+            column="e"
+            selectedSquare={selectedSquare}
+            setSelectedSquare={setSelectedSquare}
+          />
+          <Square
+            row={i}
+            column="f"
+            selectedSquare={selectedSquare}
+            setSelectedSquare={setSelectedSquare}
+          />
+          <Square
+            row={i}
+            column="g"
+            selectedSquare={selectedSquare}
+            setSelectedSquare={setSelectedSquare}
+          />
+          <Square
+            row={i}
+            column="h"
+            selectedSquare={selectedSquare}
+            setSelectedSquare={setSelectedSquare}
+          />
         </span>
-        <br/>
+        <br />
       </>
     )
   }
 
-  return (
-    <div id="board">
-      {boardData}
-    </div>
-  )
-
+  return <div id="board">{boardData}</div>
 }
 
 export default Board
