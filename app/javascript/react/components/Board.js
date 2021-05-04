d1be12b4ebee62c7b98582cd2dfb77708863369d
Michael Catanese <michaelcatanese@Catanese-Family-Computer.local>
2021-05-04 14:42:11 -0400

    pieces on board, not moveable

--- app/javascript/react/components/Board.js
+++ app/javascript/react/components/Board.js
@@ -3,6 +3,17 @@ import Square from "./Square"
 
 const Board = (props) => {
   const [selectedSquare, setSelectedSquare] = useState(null)
+  const [boardState, setBoardState] = useState({
+    gameStarted: false,
+    h: ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
+    g: ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+    f: [null, null, null, null, null, null, null, null],
+    e: [null, null, null, null, null, null, null, null],
+    d: [null, null, null, null, null, null, null, null],
+    c: [null, null, null, null, null, null, null, null],
+    b: ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+    a: ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"]
+  })
 
   let boardData = []
   let letters = ["h", "g", "f", "e", "d", "c", "b", "a"]
@@ -10,15 +21,18 @@ const Board = (props) => {
     letters.forEach((letter) => {
       boardData.unshift(
         <Square
+          key={`${letter}${i}`}
           row={i}
           column={letter}
           selectedSquare={selectedSquare}
           setSelectedSquare={setSelectedSquare}
+          boardState={boardState}
+          setBoardState={setBoardState}
         />
       )
       if (letter === "a") {
         boardData.unshift(
-          <br/>
+          <br key={`break number: ${i}`}/>
         )
       }
     })
--- app/javascript/react/components/Square.js
+++ app/javascript/react/components/Square.js
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import LightSquareImage from "../../../assets/images/light-square.png"
 import DarkSquareImage from "../../../assets/images/dark-square.png"
 
 const Square = (props) => {
+
   let id = `${props.column}${props.row}`
 
   let square = DarkSquareImage
@@ -41,13 +42,101 @@ const Square = (props) => {
     props.setSelectedSquare(id)
   }
 
+
+
+
+// CHNAGE TILE BASED ON STATE FROM BOARD, WHICH WILL CHANGE BASED ON THE CLICK OF A TILE HERE
+
+
+
+
+  let piece 
+ 
+
+
+
+  if (props.boardState.gameStarted === false) {
+    if (props.row == 2) {
+      piece = "wp"
+    }
+    if (props.row == 7) {
+      piece = "bp"
+    }
+    if (id === "a1" || id === "h1") {
+      piece = "wr"
+    }
+    if (id === "a8" || id === "h8") {
+      piece = "br"
+    }
+    if (id === "b1" || id === "g1") {
+      piece = "wn"
+    }
+    if (id === "b8" || id === "g8") {
+      piece = "bn"
+    }
+    if (id === "c1" || id === "f1") {
+      piece = "wb"
+    }
+    if (id === "c8" || id === "f8") {
+      piece = "bb"
+    }
+    if (id === "e1") {
+      piece = "wk"
+    }
+    if (id === "d1") {
+      piece = "wq"
+    }
+    if (id === "e8") {
+      piece = "bk"
+    }
+    if (id === "d8") {
+      piece = "bq"
+    }
+  }
+
+
+
+
+  let iconClass
+  if (piece === "wp") {
+    iconClass = "white fas fa-chess-pawn"
+  } else if (piece === "bp") {
+    iconClass = "fas fa-chess-pawn"
+  } else if (piece === "wr") {
+    iconClass = "white fas fa-chess-rook"
+  } else if (piece === "br") {
+    iconClass = "fas fa-chess-rook"
+  } else if (piece === "wn") {
+    iconClass = "white fas fa-chess-knight"
+  } else if (piece === "bn") {
+    iconClass = "fas fa-chess-knight"
+  } else if (piece === "wb") {
+    iconClass = "white fas fa-chess-bishop"
+  } else if (piece === "bb") {
+    iconClass = "fas fa-chess-bishop"
+  } else if (piece === "wq") {
+    iconClass = "white fas fa-chess-queen"
+  } else if (piece === "bq") {
+    iconClass = "fas fa-chess-queen"
+  } else if (piece === "wk") {
+    iconClass = "white fas fa-chess-king"
+  } else if (piece === "bk") {
+    iconClass = "fas fa-chess-king"
+  }
+
   return (
-  <img 
-    src={square} 
-    id={id}
-    className={classContent}
-    onClick={click} 
-  />)
+    <span 
+      className="square" 
+      id={id} 
+      onClick={click}
+    >
+      <img 
+        src={square} 
+        className={classContent} 
+      />
+      <i className={iconClass}></i>
+    </span>
+  )
 }
 
 export default Square
