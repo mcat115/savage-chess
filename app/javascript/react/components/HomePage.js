688bb4fa71e04198fb0a80ada82e36a9de84ba01
Michael Catanese <michaelcatanese@Catanese-Family-Computer.local>
2021-05-04 10:05:53 -0400

    simple basic style changes

--- app/javascript/react/components/HomePage.js
+++ app/javascript/react/components/HomePage.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom"
 const HomePage = (props) => {
 
   return (
-    <>
+    <div className="home-page">
       <p>Welcome to savage chess! The classic game, but with one simple twist:</p>
       <p>No checks, no mate to win. You play the extra move to kill the king! This is no longer a gentlemen’s game, this is war!</p>
       <Link to="/play">Start a game!</Link>
-    </>
+    </div>
   )
 }
 
