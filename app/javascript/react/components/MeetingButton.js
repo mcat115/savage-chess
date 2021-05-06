572b712b172cfd7b8f9adf8163c2e384426aac81
Michael Catanese <michaelcatanese@Catanese-Family-Computer.local>
2021-05-06 13:59:01 -0400

    button component complete

---
+++ app/javascript/react/components/MeetingButton.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react"
+import buttonImage from "../../../assets/images/meeting_button.png"
+
+
+const MeetingButton = () => {
+  const [message, setMessage] = useState("Call an emergency war crime meeting!")
+  const [meetingStatus, setMeetingStatus] = useState("inactive")
+
+  const click = () => {
+    if (meetingStatus === "inactive") {
+      alert("meeting animation placeholder")
+      setMessage("End the emergency war crime meeting!")
+      setMeetingStatus("active")
+    } else {
+      setMessage("Call an emergency war crime meeting!")
+      setMeetingStatus("inactive")
+      alert("If you proved your opponent cheated, X out their king with the icon bank!")
+    }
+  }
+
+  return (
+    <div id="emergencyMeetingDiv">
+      <p>{message}</p>
+      <img src={buttonImage} onClick={click}/>
+    </div>
+  )
+}
+
+export default MeetingButton
