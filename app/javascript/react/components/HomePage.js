1854e93d8f556112a868a5dc918088cad2504d74
Michael Catanese <michaelcatanese@Catanese-Family-Computer.local>
2021-05-06 00:06:16 -0400

    fixed

--- app/javascript/react/components/HomePage.js
+++ app/javascript/react/components/HomePage.js
@@ -7,8 +7,8 @@ const HomePage = (props) => {
     <div className="home-page">
       <p>Welcome to savage chess! The classic game, but with a simple twist. Inspired by the art and game elements of the viral game Among Us.</p>
       <p>No checks, no mate to win. You play the extra move to kill the king! This is no longer a gentlemen’s game, this is war!</p>
-      <p>And like real warefare, there's nobody checking that both sides follow the rules! It's up to you to notice if your opponent does anything sneaky, like trying to move a piece in a way they aren't supposed to... call a war crime trial if you have suspicions!</p>
-      <p>Or maybe you'll want to sneak in a slightly illegal move yourself when you're oppoment isn't looking... social deduction mixed with raw strategy!</p>
+      <p>And like real warefare, there's nobody checking that both sides follow the rules! It's up to you to notice if your opponent does anything sneaky, like trying to move a piece in a way they aren't supposed to... call a war crime trial if you are suspicious!</p>
+      <p>Or maybe you'll want to sneak in a slightly illegal move yourself when you're opponent isn't looking... social deduction mixed with raw strategy!</p>
       <Link to="/play">Start a game!</Link>
     </div>
   )
