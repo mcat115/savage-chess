000758a6f177b8a882e5ee32774f75041b953f5f
Michael Catanese <michaelcatanese@Catanese-Family-Computer.local>
2021-06-10 16:43:37 -0400

    DRYed up game over function

--- app/javascript/react/components/SavedStateSection.js
+++ app/javascript/react/components/SavedStateSection.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 import Save from "./Save"
 
-const SavedStatesSection = (props) => {
+const SavedStateSection = (props) => {
   const [boardStates, setBoardStates] = useState([])
   const [currentUserId, setCurrentUserId] = useState(null)
 
@@ -99,4 +99,4 @@ const SavedStatesSection = (props) => {
   }
 }
 
-export default SavedStatesSection
+export default SavedStateSection
