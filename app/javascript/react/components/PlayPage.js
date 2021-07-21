c1d26fe6e42b89610ae42e3e9833655f5fa90872
Michael Catanese <michaelcatanese@Catanese-Family-Computer.local>
2021-07-20 21:06:19 -0400

    changed meeting button to pause button

--- app/javascript/react/components/PlayPage.js
+++ app/javascript/react/components/PlayPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import Board from "./Board"
 import PieceBank from "./PieceBank"
-import MeetingButton from "./MeetingButton"
+import PauseButton from "./PauseButton"
 
 const PlayPage = () => {
   const [bankSelection, setBankSelection] = useState(null)
@@ -13,7 +13,7 @@ const PlayPage = () => {
       <div className="grid-x">
         <div className="text-center cell small-12 large-3">
           <p className="playWords">Point and click!</p>
-          <MeetingButton />
+          <PauseButton />
         </div>
         <div className="cell small-12 large-6">
           <Board
