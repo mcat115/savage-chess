71d97af9087058c15d130f49be70f325994caf43
Michael Catanese <michaeljcatanese@gmail.com>
2023-03-31 00:49:30 -0400

    Update Animation.js

--- app/javascript/react/components/Animation.js
+++ app/javascript/react/components/Animation.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Keyframes, Frame } from "react-keyframes"
+import keyframes from "react-keyframes/dist/keyframes"
 import Red from "../../../assets/images/red.png"
 import Streak1 from "../../../assets/images/streak 1.png"
 import Streak2 from "../../../assets/images/streak 2.png"
@@ -18,62 +19,56 @@ const Animation = (props) => {
     require.context("../../../assets/images/frames", false, /\.(png)$/)
   )
 
-  let frames = [[], [], [], [], [], [], [], [], [], [], [], []]
-  let allPieces = [
-    "bp",
-    "wp",
-    "bn",
-    "wn",
-    "bb",
-    "wb",
-    "br",
-    "wr",
-    "bq",
-    "wq",
-    "bk",
-    "wk",
-  ]
+  let frames = {
+    bp: [],
+    wp: [],
+    bn: [],
+    wn: [],
+    bb: [],
+    wb: [],
+    br: [],
+    wr: [],
+    bq: [],
+    wq: [],
+    bk: [],
+    wk: [],
+  }
 
   Object.keys(frameImports).forEach((key) => {
-    allPieces.forEach((piece, index) => {
+    Object.keys(frames).forEach((piece) => {
       if (key.includes(piece)) {
-        frames[index].push(frameImports[key])
+        frames[piece].push(frameImports[key])
       }
     })
   })
 
-  let animations = []
-
-  frames.forEach((piece) => {
-    let frameData = []
-    frameData.push(
-      <Frame duration={200}>
-        <img src={Red} />
-      </Frame>
-    )
+  let frameData = []
+  frameData.push(
+    <Frame duration={200}>
+      <img src={Red} />
+    </Frame>
+  )
+  frameData.push(
+    <Frame duration={100}>
+      <img src={Streak1} />
+    </Frame>
+  )
+  frameData.push(
+    <Frame duration={100}>
+      <img src={Streak2} />
+    </Frame>
+  )
+  frameData.push(
+    <Frame duration={100}>
+      <img src={Streak3} />
+    </Frame>
+  )
+  frames[props.whatShouldReturn].forEach((frame) => {
     frameData.push(
       <Frame duration={100}>
-        <img src={Streak1} />
+        <img src={frame} />
       </Frame>
     )
-    frameData.push(
-      <Frame duration={100}>
-        <img src={Streak2} />
-      </Frame>
-    )
-    frameData.push(
-      <Frame duration={100}>
-        <img src={Streak3} />
-      </Frame>
-    )
-    piece.forEach((frame) => {
-      frameData.push(
-        <Frame duration={100}>
-          <img src={frame} />
-        </Frame>
-      )
-    })
-    animations.push(frameData)
   })
 
   const newGame = () => {
@@ -81,28 +76,14 @@ const Animation = (props) => {
     props.setBoardState(props.defaultBoard)
   }
 
-  let endGameComponents = []
-
-  animations.forEach((animation) => {
-    endGameComponents.push(
-      <div>
-        <Keyframes>{animation}</Keyframes>
-        <p id="newGameButton" className="center" onClick={newGame}>
-          Start a new game!
-        </p>
-      </div>
-    )
-  })
-
-  let output
-
-  allPieces.forEach((piece, index) => {
-    if (props.whatShouldReturn === piece) {
-      output = endGameComponents[index]
-    }
-  })
-
-  return output
+  return (
+    <div>
+      <Keyframes>{frameData}</Keyframes>
+      <p id="newGameButton" className="center" onClick={newGame}>
+        Start a new game!
+      </p>
+    </div>
+  )
 }
 
 export default Animation
